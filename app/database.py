a20364ba4fa3a6801a01fe9be33f199cf6948355
chriswu727 <yichenwujob@gmail.com>
2025-02-10 00:28:18 -0600

    fixed DBAPI ERROR

--- app/database.py
+++ app/database.py
@@ -4,7 +4,8 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/cifar10db")
 
-engine = create_engine(DATABASE_URL)
+# Add pool_pre_ping=True to test connection before use
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
